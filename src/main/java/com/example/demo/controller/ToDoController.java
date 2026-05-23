d6f7afc3f347e467f4e3ac22e18f1f296b7b0054
Vitor Queiroz <vitor.qn2004@gmail.com>
2026-05-23 14:30:34 -0300

    Database:config spring database

--- src/main/java/com/example/demo/controller/ToDoController.java
+++ src/main/java/com/example/demo/controller/ToDoController.java
@@ -1,28 +1,62 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Task;
+import com.example.demo.repository.TaskRepository;
 
-import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class ToDoController {
 
-    private List<Task> tasks = new ArrayList<>();
+    private final TaskRepository taskRepository;
+
+    public ToDoController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 
     @GetMapping("/tasks")
     public List<Task> getTasks() {
-
-        return tasks;
+        return taskRepository.findAll();
     }
 
     @PostMapping("/tasks")
     public Task createTask(@RequestBody Task task) {
-        tasks.add(task);
-        return task;
-    }}
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public String deleteTask(@PathVariable Long id) {
+
+        taskRepository.deleteById(id);
+
+        return "Task deleted!";
+    }
+
+    @PutMapping("/tasks/{id}")
+    public Task updateTask(@PathVariable Long id,
+            @RequestBody Task updatedTask) {
+
+        Task task = taskRepository.findById(id).orElse(null);
+
+        if (task != null) {
+            task.setTitle(updatedTask.getTitle());
+            task.setCompleted(updatedTask.isCompleted());
+            return taskRepository.save(task);
+        }
+
+        return null;
+    }
+
+}
+
+
--- src/main/java/com/example/demo/model/Task.java
+++ src/main/java/com/example/demo/model/Task.java
@@ -1,10 +1,21 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Task {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    private boolean completed;
+
+    public Task() {
+    }
 
     public Task(Long id, String title) {
         this.id = id;
@@ -19,12 +30,20 @@ public class Task {
         return title;
     }
 
+    public boolean isCompleted() {
+        return completed;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
     }
 }
 
